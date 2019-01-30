d7c4483190b1b66f9e3a181c72489785455e7c0a
Andrey <andreinew@mail.ru>
2019-01-30 09:29:16 +0000

    Working on NavBar component

--- client/src/components/Navigation/NavBar.js
+++ client/src/components/Navigation/NavBar.js
@@ -1,23 +1,35 @@
 import React from 'react';
 import AppBar from '@material-ui/core/AppBar';
-//import { withStyle } from '@material-ui/core/styles';
+import { withStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 
-const NavBar = () => {
-  // const styles = {
+const styles = {
+  root: {
+    flexGrow: 1
+  },
+  grow: {
+    flexGrow: 1
+  },
 
-  // }
+}
+
+const NavBar = (props) => {
+
+  const { classes } = props;
   return (
-    <AppBar position='static'>
-      <Toolbar>
-        <Typography variant='title' color='inherit'>
-          Goals - Control App
-        </Typography>
-        <Button color='inherit'>Login</Button>
-      </Toolbar>
-    </AppBar>
+    <div className={classes.root}>
+      <AppBar position='static'>
+        <Toolbar>
+          <Typography variant='h6' color='inherit' className={classes.grow}>
+            Goals - Control App
+          </Typography>
+          <Button color='inherit'>Sign up</Button>
+          <Button color='inherit'>Login</Button>
+        </Toolbar>
+      </AppBar>
+    </div>
   );
 };
-export default NavBar;
+export default withStyles(styles)(NavBar);
