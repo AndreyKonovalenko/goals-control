953ecf602ce2c41aee66f2a0c720db3e5673d368
Andrey <andreinew@mail.ru>
2019-02-04 12:11:33 +0000

    Started working on draggable list with react motion library

--- client/src/components/main/Main.js
+++ client/src/components/main/Main.js
@@ -1,8 +1,25 @@
 import React, { Component } from 'react';
 import { withStyles } from '@material-ui/core/styles';
+import { Motion, spring } from 'react-motion';
+
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
+
+
+const reinsert = (arr, from, to) => {
+  const _arr = arr.slice(0);
+  const val = _arr[from];
+  _arr.splice(from, 1);
+  _arr.splice(to, 0, val);
+  return _arr;
+};
+
+const clamp = (n, min, max) => {
+  return Math.max(Math.min(n, max), min);
+};
+
+const springConfig = { stiffness: 300, dapming: 50 };
 
 const styles = theme => ({
   container: {
@@ -24,11 +41,12 @@ const styles = theme => ({
     margin: theme.spacing.unit
   }
 });
+
+
 class Main extends Component {
   // this is fake state for fronend testeing
   state = {
-    goals: [
-      {
+    goals: [{
         id: 1,
         title: 'workout 4 time a week'
       },
@@ -42,11 +60,16 @@ class Main extends Component {
       }
     ]
   };
+
+
   render() {
+    const itemsCount = this.state.goals.length;
+    console.log(itemsCount);
+
     const { classes } = this.props;
     const list = this.state.goals.map(element => (
-      <ListItem key={element.id} className={classes.list}>
-        <ListItemText primary={element.title} />
+      <ListItem key={element.id} className={classes.list} divider={true} >
+        <ListItemText primary={element.title}  />
       </ListItem>
     ));
     return (
