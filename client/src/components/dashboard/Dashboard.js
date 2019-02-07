e219487ff3ce806591a9d169e98689e3aaeec167
Andrey <andreinew@mail.ru>
2019-02-07 10:24:28 +0000

    Testing react-move library

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -1,26 +1,28 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
-import { Motion, spring } from 'react-motion';
+import { NodeGroup } from 'react-move';
+import { range } from 'd3-array';
+import { easeExpOut } from 'd3-ease';
 
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
 
+const updateOreder = (arr, beg, end) => {
+  const copy = arr.slice(0);
+  const val = copy[beg];
+  copy.splice(beg, 1);
+  copy.splice(end, 0, val);
+  return copy;
 
-const reinsert = (arr, from, to) => {
-  const _arr = arr.slice(0);
-  const val = _arr[from];
-  _arr.splice(from, 1);
-  _arr.splice(to, 0, val);
-  return _arr;
 };
 
 const clamp = (n, min, max) => {
   return Math.max(Math.min(n, max), min);
 };
 
-const springConfig = { stiffness: 300, dapming: 50 };
+const itemHeight = 75;
 
 const styles = theme => ({
   container: {
@@ -33,32 +35,31 @@ const styles = theme => ({
     width: '100%',
     backgroundColor: theme.palette.background.paper,
   },
-
-  button: {
-    margin: theme.spacing.unit
-  },
   container2: {
-    padding: '10%',
+    width: '100%',
+    height: '100%',
     display: 'flex',
     justifyContent: 'center',
-    position: 'absolute',
+    alignItems: 'center',
   },
-  root2: {
+  list: {
     width: 900,
-    backgroundColor: theme.palette.background.paper,
+    height: itemHeight * 3 // should add to inline style
   },
-
   item: {
     position: 'absolute',
     width: '100%',
-    height: 47,
+    height: `${itemHeight-3}`,
+    lineHieght: `${itemHeight-3}`,
     overflow: 'visible',
     pointerEvents: 'auto',
     transformOrigin: '50% 50% 0px',
+    color: 'red',
     borderRadius: 4,
     boxSizing: 'border-box'
-  },
+  }
 });
+
 
 class Dashboard extends Component {
   // this is fake state for fronend testeing
@@ -76,29 +77,30 @@ class Dashboard extends Component {
         title: 'Fide the right pass'
       }
     ],
-    order: [0, 1, 2],
-    itemsCount: 3,
+    order: [0, 1, 2], // from back-end
+    itemsCount: 3, // form back- end
     topDeltaY: 0,
     mouseY: 0,
     isPressed: false,
-    originalPosOfLastPressed: 0,
+    lastPressed: 0,
     animation: true
   };
 
-  componentDidMount() {
+  handleTouchStart = (pos, pressY, { touches: [{ pageY }] }) => {
+    this.setState({
+      topDeltaY: pageY - pressY,
+      mouseY: pressY,
+      isPressed: true,
+      lastPressed: pos
+    });
+
     window.addEventListener('touchmove', this.handleTouchMove);
-    window.addEventListener('touchend', this.handleMouseUp);
-    window.addEventListener('mousemove', this.handleMouseMove);
-    window.addEventListener('mouseup', this.handleMouseUp);
+    window.addEventListener('touchend', this.handleTouchEnd);
   };
 
-  handleTouchStart = (key, pressLocation, e) => {
-    this.handleMouseDown(key, pressLocation, e.touches[0]);
-  };
-
-  handleTouchMove = (e) => {
+  handleTouchMove = e => {
     e.preventDefault();
-    this.handleMouseMove(e.touches[0]);
+    this.handleTouchMove(e.touches[0]);
   };
 
   handleMouseDown = (pos, pressY, { pageY }) => {
@@ -106,73 +108,99 @@ class Dashboard extends Component {
       topDeltaY: pageY - pressY,
       mouseY: pressY,
       isPressed: true,
-      originalPosOfLastPressed: pos,
+      lastPressed: pos
     });
+
+    window.addEventListener("mousemove", this.handleMouseMove);
+    window.addEventListener("mouseup", this.handleMouseUp);
   };
 
-  handleMouseMove = ({ pageY }) => {
-    const { isPressed, topDeltaY, order, originalPosOfLastPressed, itemsCount } = this.state;
+  handleTouchMove = ({ pageY }) => {
+    const { isPressed, topDeltaY, lastPressed, itemsCount, order } = this.state;
 
     if (isPressed) {
       const mouseY = pageY - topDeltaY;
-      const currentRow = clamp(Math.round(mouseY / 100), 0, itemsCount - 1);
+      const currentRow = clamp(
+        Math.round(mouseY / itemHeight),
+        0,
+        itemsCount - 1
+      );
       let newOrder = order;
-
-      if (currentRow !== order.indexOf(originalPosOfLastPressed)) {
-        newOrder = reinsert(order, order.indexOf(originalPosOfLastPressed), currentRow);
+      if (currentRow !== order.indexOf(lastPressed)) {
+        newOrder = updateOreder(order, order.indexOf(lastPressed), currentRow);
       }
-
-      this.setState({ mouseY: mouseY, order: newOrder });
+      this.setState({ mouseY, order: newOrder });
     }
   };
 
   handleMouseUp = () => {
     this.setState({ isPressed: false, topDeltaY: 0 });
+    window.removeEventListener('mouseup', this.handleMouseUp);
+    window.removeEventListener('mousemove', this.handleTouchMove);
+  };
+
+  handleTouchEnd = () => {
+    this.setState({ isPressed: false, topDeltaY: 0 });
+    window.removeEventListener('touchmove', this.handleTouchMove);
+    window.removeEventListener('touchend', this.handleTouchEnd);
   };
 
   render() {
-    const { isPressed, mouseY, order, originalPosOfLastPressed } = this.state;
-    const { classes } = this.props;
+
     let list;
+    const { classes } = this.props;
+    const { mouseY, isPressed, lastPressed, order, itemsCount } = this.state;
 
-    // Animation List
-    const listRootHeight = this.state.itemsCount * 47; // animation style config
-
-
-    const animatedList = order.map(i => {
-      const style = originalPosOfLastPressed === i && isPressed ? {
-        scale: spring(1.1, springConfig),
-        shadow: spring(16, springConfig),
-        y: mouseY,
-      } : {
-        scale: spring(1, springConfig),
-        shadow: spring(1, springConfig),
-        y: spring(order.indexOf(i) * 100, springConfig),
-      };
-
-      return (
-        <div className={classes.container2} >
-          <List className={classes.root2} style={{height:`${listRootHeight}px`}}>
-            <Motion style={style} key={i}>
-                {({scale, shadow, y}) =>
-                    <ListItem
-                      onMouseDown={this.handleMouseDown.bind(null, i, y)}
-                      onTouchStart={this.handleTouchStart.bind(null, i, y)}
-                      className={classes.item}
-                      style={{
-                        boxShadow: `rgba(0, 0, 0, 0.2) 0px ${shadow}px ${2 * shadow}px 0px`,
-                        transform: `translate3d(0, ${y}px, 0) scale(${scale})`,
-                        WebkitTransform: `translate3d(0, ${y}px, 0) scale(${scale})`,
-                        zIndex: i === originalPosOfLastPressed ? 99 : i,
-                        }}>
-                      <ListItemText primary={this.state.goals[i].title}  />
-                    </ListItem>
-                }
-              </Motion>
-            </List>
-          </div>
-      );
-    });
+    const animatedList = (
+      <div className={classes.container2}>
+        <NodeGroup
+          data={range(itemsCount)} // this is parameter sets the number of nodes
+          keyAccessor={d => `item-key-${d}`}
+          start={d => ({
+            scale:1,
+            shadow:1,
+            y: order.indexOf(d)*itemHeight
+          })}
+          update={d => {
+            const dragging = lastPressed === d && isPressed;
+            return {
+              scale: [dragging ? 1.1 : 1],
+              shadow: [dragging ? 5: 1],
+              y: [order.indexOf(d) * itemHeight],
+              timing: { duration: 350, ease: easeExpOut}
+            };
+          }}
+        >
+          {nodes => (
+            <div className={classes.list}>
+              {nodes.map(({key, data, state }) => {
+                console.log(nodes);
+                const {shadow, scale, y} = state;
+                const transY = lastPressed === data && isPressed ? mouseY : y;
+                return (
+                  <div
+                    className={classes.item}
+                    key={key}
+                    onMouseDown={e => this.handleMouseDown(data, y, e)}
+                    onTouchStart={e => this.handleTouchStart(data, y, e)}
+                    style={{
+                      boxShadow: `rgba(0, 0, 0, 0.4) 0px ${shadow}px ${2 *
+                        shadow}px 0px`,
+                      transform: `translate3d(0, ${transY}px, 0) scale(${scale})`,
+                      WebkitTransform: `translate3d(0, ${transY}px, 0) scale(${scale})`,
+                      zIndex: data === lastPressed ? 99 : data
+                    }}
+                  >
+                    {order.indexOf(data) +1}
+                    {this.state.goals[order.indexOf(data)].title}
+                  </div>
+                );
+              })}
+            </div>
+          )}
+        </NodeGroup>
+      </div>
+    );
 
     const basicList = (
       <div className={classes.container}>
@@ -193,8 +221,8 @@ class Dashboard extends Component {
     else {
       list = basicList;
     }
-
-    return list;
+    console.log(animatedList);
+    return animatedList;
   }
 }
 
