dcfe0ec48e9fd58780526ede1c668f18393b98e6
AndreyKonovalenko <andreinew@mail.ru>
2019-02-12 19:12:19 +0300

    Created and setup store for redux

--- client/src/store/store.js
+++ client/src/store/store.js
@@ -1,11 +1,18 @@
 import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
 import thunk from 'redux-thunk';
-const composeEnhancers = process.env.NODE_ENV === 'development' ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ : null || compose;
 import dashboardReducer from './reducers/dashboardReducer';
+
+const composeEnhancers =
+  process.env.NODE_ENV === 'development'
+    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    : null || compose;
 
 const rootReducer = combineReducers({
   dashboard: dashboardReducer
 });
-const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)));
+const store = createStore(
+  rootReducer,
+  composeEnhancers(applyMiddleware(thunk))
+);
 
 export default store;
