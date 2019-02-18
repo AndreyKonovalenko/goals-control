6160022f4fc64e52e6777f3b6725e08c410118e7
Andrey <andreinew@mail.ru>
2019-02-18 12:24:02 +0000

    Added draft weekdays style

--- client/src/components/calendar/WeekDays.js
+++ client/src/components/calendar/WeekDays.js
@@ -9,36 +9,34 @@ import dateFns from 'date-fns';
 
 const styles = (theme) => ({
   root: {
-
-    [theme.breakpoints.down('xs')]: {
-      width: '100%', // for screens smaller then 600 use 100%
-    },
-    backgroundColor: theme.palette.background.paper,
     display: 'flex',
     justifyContent: 'center',
     flexDirection: 'row',
-    alignItems: 'center',
   },
+  text: {
+    textAlign: 'center',
+    padding: 0
+  }
 });
 
 
 const WeekDays = (props) => {
   const { classes } = props;
   const days = [];
-  let dateFormat = "dd";
+  let dateFormat = "ddd";
   let startDate = dateFns.startOfWeek(props.currentMonth, { weekStartsOn: 1 });
 
   for (let i = 0; i < 7; i++) {
     days.push(
       <ListItem key={i}>
-        <ListItemText>
+        <ListItemText className={classes.text}>
          {dateFns.format(dateFns.addDays(startDate, i), dateFormat)}
         </ListItemText>
       </ListItem>
     );
   }
   return (
-    <List className={classes.root}>
+    <List className={classes.root} disablePadding={true}>
       {days}
     </List>
   );
