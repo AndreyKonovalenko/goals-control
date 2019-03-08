0e4bc192245b79d70b0eed0257517bada4ac8991
AndreyKonovalenko <andreinew@mail.ru>
2019-03-08 18:18:31 +0300

    Added AutoLogin logic in App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-//import jwt_decode from 'jwt_decode';
+import jwt_decode from 'jwt-decode';
 import { Route, Switch, withRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
 
@@ -13,36 +13,33 @@ import CssBaseline from '@material-ui/core/CssBaseline';
 import Calendar from './components/calendar/Calendar';
 import NotFound from './components/notfound/NotFound';
 import Welcome from './components/welcome/Welcome';
-//import setAuthToken from './utils/setAuthToken';
-//import { setCurrentUser, logoutUser } from './actions/authActions';
+import setAuthToken from './utils/setAuthToken';
+import { setCurrentUser, logoutUser } from './store/actions/authActions';
+
+import store from './store/store';
 
 console.log(localStorage);
-// // Check for token
-// if (localStorage.jwtToken) {
-//   // Set auth token header auth
-//   setAuthToken(localStorage.jwtToken);
-//   // Decode token and get user info ad expiration
-//   const decoded = jwt_decode(localStorage.jwtToken);
-//   // Set user and isAthenticated
+// Check for token
+if (localStorage.jwtToken) {
+  // Set auth token header auth
+  setAuthToken(localStorage.jwtToken);
+  // Decode token and get user info ad expiration
+  const decoded = jwt_decode(localStorage.jwtToken);
+  // Set user and isAthenticated
 
-//   store.dispatch(setCurrentUser(decoded));
+  store.dispatch(setCurrentUser(decoded));
 
-//   // Check for expired token
-//   const currentTime = Date.now() / 1000;
-//   if (decoded.exp < currentTime) {
-//     // Logout user
-//     store.dispatch(logoutUser());
-//     // Clear current Profile
-//     store.dispatch(clearCurrentProfile());
-//     // Redirect to login
-//     window.location.href = '/login';
-//   }
-// }
+  // Check for expired token
+  const currentTime = Date.now() / 1000;
+  if (decoded.exp < currentTime) {
+    // Logout user
+    store.dispatch(logoutUser());
+    // Clear current Profile
+    window.location.href = '/login';
+  }
+}
 
 class App extends Component {
-  componentDidMount() {
-    console.log('App.js Mounted');
-  }
   render() {
     let routes;
     const { isAuthenticated } = this.props.auth;
